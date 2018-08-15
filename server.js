578703a55596d3e6d104db7a46d4dab9a90eb306
Rich <rmrouse88@gmail.com>
2018-08-15 11:17:30 -0500

    upgrade to Angular 6 and ready for initial production commit

--- server.js
+++ server.js
@@ -3,20 +3,9 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const proxy = require('http-proxy-middleware')
 const Twitter = require('twitter')
-const rx = require('rxjs')
+// const rx = require('rxjs')
 
 require('dotenv').config()
-
-client = new Twitter({
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token_key: process.env.ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET  
-})
-
-let params = {track:'javascript', filter_level: 'none'}
-    
-let stream = client.stream('statuses/filter', params)
 
 const app = express()
 
