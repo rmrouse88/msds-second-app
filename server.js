577c7688be47a9fdeb6e371e59d604aed11ef508
Rich <rmrouse88@gmail.com>
2018-08-15 09:53:01 -0500

    initial production commit

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const proxy = require('http-proxy-middleware')
+const Twitter = require('twitter')
+const rx = require('rxjs')
 
-// const myObserver = Rx.Observable.create((observer, params) => {
-//     observer.next(request.get())
-// })
+require('dotenv').config()
+
+client = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET  
+})
+
+let params = {track:'javascript', filter_level: 'none'}
+    
+let stream = client.stream('statuses/filter', params)
 
 const app = express()
 
@@ -27,4 +38,11 @@ let options = {
 let myProxy = proxy(options)
 
 app.use('/predict*', myProxy)
+
+let twitter_proxy = {
+    "target": ""
+}
+
+// app.use('/twitter'
+// )
 
